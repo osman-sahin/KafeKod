d4095202863a460a1932ced36e0fd9c77b5a01bb
Osman Şahin <osmansahin.eng@gmail.com>
2020-02-17 21:30:27 +0300

    Sınıflar

---
+++ KafeKod.Data/SiparisDetay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafeKod.Data
+{
+    public class SiparisDetay
+    {
+        public string UrunAd { get; set; }
+        public int Adet { get; set; }
+        public decimal BirimFiyat { get; set; }
+
+        //public decimal Tutar()
+        //{
+        //    return Adet * BirimFiyat;
+        //}
+
+        public decimal Tutar() => Adet * BirimFiyat;   // ()siz yaparsak sadece get'i olan bi property olur.
+    }
+}
